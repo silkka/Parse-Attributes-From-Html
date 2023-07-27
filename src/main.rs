46f7d31e4af1526faf0d3f949f2b230e0d7c28aa
Silkka <silkkaplz@gmail.com>
2023-07-27 20:52:17 +0300

    Works

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
 use std::str::from_utf8;
 
 fn main() {
-    use quick_xml::events::Event;
-    use quick_xml::reader::Reader;
-
     let xml = r#"<tag1 data-ui-id="test" data-ui-content="lol">
                 <tag2><!--Test comment-->Test</tag2>
                 <tag2 data-ui-id="test1" data-ui-content="lol1">Test 2</tag2>
              </tag1>"#;
+
+    let result = parse(xml);
+    println!("{result}");
+}
+
+fn parse(xml: &str) -> String {
     let mut reader = Reader::from_str(xml);
     reader.trim_text(true);
-
     let mut buf = Vec::new();
     let mut result: Vec<String> = Vec::new();
 
-    // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
     loop {
         match reader.read_event_into(&mut buf) {
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
@@ -36,6 +39,5 @@ fn main() {
             break;
         };
     }
-
-    println!("{:?}", result)
+    return result.concat();
 }
